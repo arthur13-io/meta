0274fcb51ad6daf1b05ed51553e145e934f3f24f
huncho-hackz <davart776@gmail.com>
2023-01-07 00:31:45 +0000

    error 404 page

---
+++ business/error404.js
@@ -0,0 +1,8 @@
+let container = document.getElementById('container');
+window.onmousemove = function(e){
+    let x = + e.clientX/5, 
+    y= - e.clientY/5;
+    container.style.backgroundPositionX = x + 'px';
+    container.style.backgroundPositionY = y + 'px';
+
+}
